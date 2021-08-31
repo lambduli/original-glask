8d19f8c1fbed24a55e5b9249ab0ab95ae5c31d06
Taskkill <johny95wil@gmail.com>
2021-08-31 12:24:02 +0200

    add another part of the implementation of To'AST

--- src/Compiler/Syntax/ToAST.hs
+++ src/Compiler/Syntax/ToAST.hs
@@ -8,9 +8,11 @@ import Data.List (intersperse)
 
 
 import Compiler.Syntax.Term
+import qualified Compiler.Syntax.Term as Term
 import Compiler.Syntax.Term.Expression
 
 import Compiler.Syntax
+import qualified Compiler.Syntax as AST
 import Compiler.Syntax.Expression
 
 import Compiler.Syntax.ToAST.Translate
@@ -121,12 +123,83 @@ instance To'AST a b => To'AST (Name, a) (Name, b) where
 
 
 instance To'AST Term'Type Type where
-  to'ast = undefined
+  to'ast (Term'T'Id (Term'Id'Var var)) = undefined
+
+  to'ast (Term'T'Id (Term'Id'Const con)) = undefined
+
+  to'ast (Term'T'Tuple t'types) = undefined
+
+  to'ast (Term'T'List t'type) = undefined
+  
+  {- This relies on the fact that (->) is registered in the fixity environment
+      and the shunting yard algorithm is implemented for types too.
+      
+    In case I change my mind about that -> just do it the simple way. -}
+  to'ast (Term'T'Arrow t'types) = do
+    to'ast $ Term'T'App $ intersperse (Term'T'Id (Term'Id'Const "(->)")) t'types
+
+  to'ast (Term'T'App t'types) = undefined
+  -- TODO: implement shunting yard algorithm
 
 
 instance To'AST Term'Pred Predicate where
-  to'ast = undefined
+  to'ast (Term.Is'In name t'type) = do
+    type' <- to'ast t'type
+    return $ AST.Is'In name type'
 
 
+-- NOTE: Data declarations and Type Synonyms will introduce new local kind variables for new local type variables
 instance To'AST Term'Decl Declaration where
-  to'ast = undefined
+  {- TODO: implement ... this one is going to be tricky -}
+  -- I am going to need to produce Bind'Group - at this stage it will just contain a single match
+  
+  -- Match contains list of Patterns and rhs :: Expression
+  -- t'expr is going to be translated directly to be the rhs
+  -- t'pat must first be translated and then reconstructed - the top level node of the pattern will become a name of the Bind'Group
+  -- the rest of it will become a part of the singleton [Match]
+
+  -- THERE IS A NOTE IN THE FORM OF A QUESTION IN THE MODULE FOR MATCH
+  -- PLEASE CONSULT IT FIRST, THINK ABOUT IT AND FIND A SATISFIABLE ANSWER
+  to'ast (Term.Binding t'pat t'expr) = undefined
+
+  to'ast (Term.Signature name t'qual'type) = do
+    qual'type <- to'ast t'qual'type
+    return $ AST.Signature $ AST.T'Signature name qual'type
+
+  to'ast (Term.Data'Decl name params t'constr'decls) = do
+    constr'decls <- to'ast t'constr'decls
+    return $ AST.Data'Decl name params constr'decls
+
+  to'ast (Term.Type'Alias name params t'type) = do
+    type' <- to'ast t'type
+    return $ AST.Type'Alias name params type'
+
+  to'ast (Term.Fixity fixity level name) = do
+    return $ AST.Fixity fixity level name
+
+  to'ast (Term.Class cl'name var'name t'preds t'decls) = do
+    preds <- to'ast t'preds
+    decls <- to'ast t'decls
+    return $ AST.Class cl'name var'name preds decls
+
+  to'ast (Term.Instance t'qual'pred t'decls) = do
+    qual'pred <- to'ast t'qual'pred
+    decls <- to'ast t'decls
+    return $ AST.Instance qual'pred decls
+
+
+instance (To'AST a b) => To'AST ([Term'Pred], a) (Qualified b) where
+  to'ast (t'preds, a) = do
+    preds <- to'ast t'preds
+    b <- to'ast a
+    return $ preds :=> b
+
+
+instance To'AST Term'Constr'Decl Constr'Decl where
+  to'ast (Term.Con'Decl name t'types) = do
+    types <- to'ast t'types
+    return $ AST.Con'Decl name types
+
+  to'ast (Term.Con'Record'Decl name t'field'types) = do
+    field'types <- to'ast t'field'types
+    return $ AST.Con'Record'Decl name field'types
