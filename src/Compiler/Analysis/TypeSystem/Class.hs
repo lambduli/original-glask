e6f3068f0cfb1ee006ec194ab30556331103e48c
Taskkill <johny95wil@gmail.com>
2021-09-03 16:58:53 +0200

    add initial implementation for Classes and utility functions around class checking

---
+++ src/Compiler/Analysis/TypeSystem/Class.hs
@@ -0,0 +1,17 @@
+module Compiler.Analysis.TypeSystem.Class where
+
+
+
+import qualified Data.Map.Strict as Map
+import Data.Maybe
+import Control.Monad.Except
+import Control.Monad.Extra
+import Control.Monad.Trans.Except (catchE)
+
+
+import Compiler.Syntax
+
+type Supers = [Name]
+
+
+type Class = (Supers, [Instance])
