f0f50ed005c9e11275715b3c087a8cfa93f1eedf
Taskkill <johny95wil@gmail.com>
2021-09-08 10:25:26 +0200

    add an empty module with some comments

---
+++ src/Compiler/Analysis/Semantic/SynonymAnalysis.hs
@@ -0,0 +1,11 @@
+module Compiler.Analysis.Semantic.SynonymAnalysis where
+
+
+-- | TODO: check for Synonym cycles
+
+-- | TODO:  consider collecting type constraints from the program containing synonyms too
+--          maybe it could give some nicer error messages?
+--          just think about it and write the result somewhere
+
+
+-- | TODO: expand synonyms (not here, but somewhere)
