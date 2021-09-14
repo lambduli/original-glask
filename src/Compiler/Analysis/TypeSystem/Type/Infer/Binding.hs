abf527c598bd63ae20c0e528ea57a0f691c8a432
Taskkill <johny95wil@gmail.com>
2021-09-14 17:10:08 +0200

    implement inference for implicit bindings

--- src/Compiler/Analysis/TypeSystem/Type/Infer/Binding.hs
+++ src/Compiler/Analysis/TypeSystem/Type/Infer/Binding.hs
@@ -8,7 +8,7 @@ import Data.Functor.Identity
 
 import qualified Data.Set as Set
 
-import Data.List ((\\))
+import Data.List
 
 
 import Compiler.Syntax
@@ -40,6 +40,7 @@ import Compiler.Analysis.TypeSystem.Type.Infer.Match
     This means, that each explicit can be infered and solved immidiately. That is what we are going to do.
 
 -}
+{- Returning a [Constraint Type] might not be strictly necessary -}
 infer'expl :: Explicit -> Infer ([Predicate], [Constraint Type], [Constraint Kind])
 infer'expl (Explicit scheme bg@Bind'Group{ name = name, alternatives = matches }) = do
   (qs :=> t) <- instantiate scheme
@@ -52,7 +53,7 @@ infer'expl (Explicit scheme bg@Bind'Group{ name = name, alternatives = matches }
           t'  = apply subst t
           -- so I need to apply the substitution to the typing context
           -- because that is how it's done in the THIH paper
-      Infer'Env{ kind'env = k'env, type'env = t'env, class'env = c'env } <- ask
+      Infer'Env{ type'env = t'env, class'env = c'env } <- ask
       let fs = Set.toList $ free'vars $ apply subst t'env
           gs = Set.toList (free'vars t') \\ fs
       let sc' = close'over (qs' :=> t')
@@ -72,3 +73,51 @@ infer'expl (Explicit scheme bg@Bind'Group{ name = name, alternatives = matches }
               else if not (null retained'preds)
               then throwError Context'Too'Weak
               else return (deferred'preds, cs't, cs'k)
+
+
+{- Returning a [Constraint Type] might not be strictly necessary -}
+infer'impls :: [Implicit] -> Infer ([Predicate], [(Name, Scheme)], [Constraint Type], [Constraint Kind])
+infer'impls implicits = do
+  let is = map (\ (Implicit b'g) -> name b'g) implicits
+      make'fresh'tv _ = do fmap (\ t'name -> T'Var (T'V t'name K'Star)) fresh
+  ts <- mapM make'fresh'tv is
+  let scs = map to'scheme ts
+      assumptions = zip is scs -- NOTE: I will need to put this into the typing context later
+      many'matches = map (\ (Implicit b'g) -> alternatives b'g) implicits
+  -- now I am going to merge them into the typing context
+  -- explanation of the zipWithM part: I need to infer list of matches and I also need to
+  -- make sure that once inference for every match is done, constraint unifying the resulting type
+  -- with the original fresh type variable will be created and registered
+  -- results :: [([Predicate], [Constraint Type], [Constraint Kind])] -- for better reading experience
+  results <- merge'into't'env assumptions $ zipWithM infer'matches many'matches ts
+  let preds = concat [ preds  | (preds, _, _) <- results ]
+      cs't  = concat [ cs't   | (_, cs't, _) <- results ]
+      cs'k  = concat [ cs'k   | (_, _, cs'k) <- results ]
+  
+  -- now I can solve the type constraints
+  case run'solve cs't :: Either Error (Subst T'V Type) of
+    Left err -> throwError err
+    Right subst -> do
+      let preds'  = apply subst preds
+          ts'     = apply subst ts
+      Infer'Env{ type'env = t'env, class'env = c'env } <- ask
+      let fs = Set.toList $ free'vars $ apply subst t'env
+          vss = map (Set.toList . free'vars) ts'
+          gs = foldr1 union vss \\ fs
+      case runIdentity $ runExceptT $ split c'env fs (foldr1 intersect vss) preds' of
+        Left err -> throwError err
+        Right (deferred'preds, retained'preds) -> do
+          if restricted implicits then
+            let gs'   = gs \\ Set.toList (free'vars retained'preds)
+                scs'  = map (quantify gs' . ([] :=> )) ts'
+            in return (deferred'preds ++ retained'preds, zip is scs', cs't, cs'k)
+          else
+            let scs'  = map (quantify gs . (retained'preds :=> )) ts'
+            in return (deferred'preds, zip is scs', cs't, cs'k)
+
+
+restricted :: [Implicit] -> Bool
+restricted implicits = any simple alts
+  where
+    alts = map (\ (Implicit b'g) -> alternatives b'g) implicits
+    simple alts = any (null . patterns) alts
