5b6c34d424f76144c80557ac67b6577aada3f3a4
Taskkill <johny95wil@gmail.com>
2021-11-14 13:05:33 +0100

    complete Show implementation

--- src/Compiler/Syntax/Term/Pattern.hs
+++ src/Compiler/Syntax/Term/Pattern.hs
@@ -1,5 +1,7 @@
 module Compiler.Syntax.Term.Pattern where
 
+import Data.List.Extra (intercalate)
+
 
 import Compiler.Syntax.Name
 import Compiler.Syntax.Literal
@@ -30,8 +32,21 @@ instance Show Term'Pat where
   show (Term'P'Lit lit) =
     show lit
 
-  show (Term'P'App term'patterns) = show term'patterns
+  show (Term'P'App term'patterns) = unwords $ map show term'patterns
 
-  show (Term'P'Labeled _ _) = "[[Pattern Labeled]]"
+  show (Term'P'Labeled con'name field'patterns) =
+    let
+      show'field'patterns (name, term'pat) = name ++ " = " ++ show term'pat
+    in
+      con'name ++ "{ " ++ unwords (map show'field'patterns field'patterns) ++ " }"
 
-  show _ = "Not Implemented: Show for Term'Pat"
+  show (Term'P'Tuple term'patterns) =
+    "(" ++ intercalate ", " (map show term'patterns) ++ ")"
+
+  show (Term'P'List term'patterns) = show term'patterns
+
+  show (Term'P'As name term'pat) =
+    name ++ "@" ++ show term'pat
+
+  show Term'P'Wild =
+    "_"
