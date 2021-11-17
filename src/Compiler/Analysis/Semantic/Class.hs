b3eb2975b0ca81e27e25f65393b4e37a1104a2dc
Taskkill <johny95wil@gmail.com>
2021-11-17 15:49:54 +0100

    implements building a class environment for the type inference

---
+++ src/Compiler/Analysis/Semantic/Class.hs
@@ -0,0 +1,45 @@
+module Compiler.Analysis.Semantic.Class where
+
+import qualified Data.Map.Strict as Map
+import Data.Bifunctor
+
+import Compiler.Syntax
+
+import Compiler.Analysis.TypeSystem.Class
+import Compiler.Analysis.TypeSystem.InferenceEnv
+
+
+{- This module builds a Class'Env for the Type Inference
+    it needs [Declaration] and produces Class'Env
+-}
+
+
+analyze :: [Declaration] -> Class'Env
+analyze declarations = Class'Env{ classes = classes, defaults = [] } -- TODO: add actual defaults as needed
+  where
+    is'class :: Declaration -> Bool
+    is'class (Class _ _ _ _) = True
+    is'class _ = False
+
+    is'instance :: Declaration -> Bool
+    is'instance (Instance _ _) = True
+    is'instance _ = False
+
+    cls'lst :: [(Name, (Supers, [Instance]))]
+    cls'lst = map (\ (Class name var preds decls) -> (name, (supers preds, []))) $ filter is'class declarations
+
+    supers :: [Predicate] -> [Name]
+    supers = map (\ (Is'In name type') -> name) -- where type' is assumed to only ever be a Type Variable
+
+    instances :: [Instance]
+    instances = map (\ (Instance qual'pred _) -> qual'pred) $ filter is'instance declarations
+
+    build'class'map :: [Instance] -> Map.Map Name Class
+    build'class'map instances = go instances $ Map.fromList cls'lst
+      where
+        go :: [Instance] -> Map.Map Name Class -> Map.Map Name Class
+        go [] classes = classes
+        go (inst@(_ :=> (Is'In name type')) : insts) classes = go insts $ Map.adjust (second (inst :)) name classes
+        -- where type' is assumed to only ever be a Type Variable
+
+    classes = build'class'map instances
