b2eb2c2d9fece150dca3738ea3b8d56ef528cb7f
Taskkill <johny95wil@gmail.com>
2022-01-31 19:58:36 +0100

    add (list of) Methods to the Program

--- src/Compiler/Analysis/TypeSystem/Binding.hs
+++ src/Compiler/Analysis/TypeSystem/Binding.hs
@@ -2,9 +2,21 @@ module Compiler.Analysis.TypeSystem.Binding where
 
 
 import Compiler.Syntax
+import Compiler.Analysis.TypeSystem.Solver.Substitutable (Subst)
 
 
 newtype Implicit = Implicit Bind'Group
+  deriving (Show)
 
 
 data Explicit = Explicit Scheme Bind'Group
+  deriving (Show)
+
+
+{-  
+  Scheme - Annotation from the Type Class definition
+  Subst T'V Type - Singleton substitution mapping a Type Class parameter/variable to the Instance defined Type
+  Bind'Group - The method implementation itself.
+-}
+data Method = Method Scheme (Subst T'V Type) Bind'Group
+  deriving (Show)
--- src/Compiler/Analysis/TypeSystem/Program.hs
+++ src/Compiler/Analysis/TypeSystem/Program.hs
@@ -2,6 +2,19 @@ module Compiler.Analysis.TypeSystem.Program where
 
 
 import Compiler.Analysis.TypeSystem.BindSection (Bind'Section)
+import Compiler.Analysis.TypeSystem.Binding
 
 
-type Program = [Bind'Section]
+{-  NOTE: I am looking at the definition of `Program` and thinking.
+          It seems like this is yet another representation - very similar to what I already have,
+          but specific in information I would be carrying around.
+          I obviously appreciate not having to carry the whole list of Declarations.
+          I also realize I will need to constantly build these "specific" representations.
+          I will also need to maintain all of the code created to specify them.
+          I should constantly be weighting the pros and cons.
+          If there's more cons to have it be "specific"
+          I should just make that part of the system work with the previous and already-defined representation.
+-}
+
+data Program = Program{ bind'sections :: [Bind'Section], methods :: [Method] }
+  deriving (Show)
