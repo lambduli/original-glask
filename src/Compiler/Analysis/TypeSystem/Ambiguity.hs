37fd840fad6bbd1ea845a79dc81502a6a100c6f4
Taskkill <johny95wil@gmail.com>
2022-01-31 20:36:46 +0100

    fix some bugs

--- src/Compiler/Analysis/TypeSystem/Ambiguity.hs
+++ src/Compiler/Analysis/TypeSystem/Ambiguity.hs
@@ -4,6 +4,7 @@ module Compiler.Analysis.TypeSystem.Ambiguity where
 import Data.Set (difference, fromList, toList)
 import Data.List (filter)
 import Data.Foldable.Extra (allM)
+import Control.Monad (filterM)
 
 import Compiler.Syntax
 
@@ -27,7 +28,7 @@ ambiguities cl'env vars preds = [(v, filter (elem v . free'vars) preds) | v <- t
 
 num'classes :: [Name]
 num'classes
-  = ["Eq", "Integral", "Floating", "Fractional", "Real", "RealFloat", "RealFrac"]
+  = ["Num", "Integral", "Floating", "Fractional", "Real", "RealFloat", "RealFrac"]
 
 
 std'classes :: [Name]
@@ -37,13 +38,25 @@ std'classes
 
 
 {- TODO: refactor a little -}
+{-  TODO: I have seriously messed up this function.
+          First of all - the original was wrong.
+          Now I have hopefully fixed it. But I need to rewrite it and remove the traces.
+          While doing so - really carefully check that it's correct.
+-}
 candidates :: Class'Env -> Ambiguity -> Solve [Type]
 candidates cl'env (v, qs) = do
-  let is = [i | Is'In i t <- qs]
-  let ts = [t | Is'In i t <- qs]
-  let ts' = [t' | all (T'Var v ==) ts,
-                  any (`elem` num'classes) is,
-                  all (`elem` std'classes) is,
+  let is = [i | Is'In i t <- qs] -- all Class names from the constraints
+  let ts = [t | Is'In i t <- qs] -- all types/parameters of the constraints
+  let ts' = [t' | all (T'Var v ==) ts, -- all the types must be just a `Type Variable v`
+                  any (`elem` num'classes) is, -- at least one of the classes must be standard numerical class
+                  all (`elem` std'classes) is, -- all of them are standard classes
                   t' <- defaults cl'env]
-  ok <- allM (entail cl'env []) [Is'In i t' | i <- is, t' <- ts']
-  return $ if ok then ts' else []
+  let
+    entails :: Type -> Solve Bool
+    entails t =
+      let
+        constraints :: [Predicate] 
+        constraints = [Is'In i t | i <- is]
+      in allM (entail cl'env []) constraints
+  filterM entails ts' -- [Is'In i t' | i <- is, t' <- aa]
+  -- return $ if ok then ts' else []
