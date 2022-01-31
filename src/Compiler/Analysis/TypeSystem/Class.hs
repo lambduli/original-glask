e50ef775b6b26ceb3c7f6b35d68a7dd98406822f
Taskkill <johny95wil@gmail.com>
2022-01-31 21:56:09 +0100

    reformat

--- src/Compiler/Analysis/TypeSystem/Class.hs
+++ src/Compiler/Analysis/TypeSystem/Class.hs
@@ -2,10 +2,6 @@ module Compiler.Analysis.TypeSystem.Class where
 
 
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Control.Monad.Except
-import Control.Monad.Extra
-import Control.Monad.Trans.Except (catchE)
 
 
 import Compiler.Syntax.Name
