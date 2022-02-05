ac03b9535e7b73ac6bbccb0e48d39362ce18ced6
Taskkill <johny95wil@gmail.com>
2022-02-05 16:42:19 +0100

    remove the comment - it was not right

--- src/Compiler/Analysis/TypeSystem/Class.hs
+++ src/Compiler/Analysis/TypeSystem/Class.hs
@@ -11,6 +11,4 @@ import Compiler.Syntax.Instance
 type Supers = [Name]
 
 
--- TODO: Question:  Should I also add a `Name` parameter denoting the name of the class type variable?
---                  I am going to need it for method elaboration - to construct the trivial substitution between that exact variable and the Instance Type.
 type Class = (Supers, [Instance])
