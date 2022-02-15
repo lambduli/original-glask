071c08ed0adb078e2ddacc2485fb1150fd11a44f
Taskkill <johny95wil@gmail.com>
2022-02-15 12:15:03 +0100

    implement Show for the Constraint - to get better readability from the debugging output

--- src/Compiler/TypeSystem/Constraint.hs
+++ src/Compiler/TypeSystem/Constraint.hs
@@ -3,3 +3,7 @@ module Compiler.TypeSystem.Constraint where
 
 data Constraint a
   = Unify a a
+
+
+instance (Show a) => Show (Constraint a) where
+  show (a `Unify` b) = " " ++ show a ++ " Unify " ++ show b ++ " "
