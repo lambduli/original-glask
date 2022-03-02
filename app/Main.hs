fcd588909a796bac79416e8c0db0b3afeb1911a5
Taskkill <johny95wil@gmail.com>
2022-03-02 20:35:12 +0100

    allow loading arbitrary module using command line argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,24 @@
 module Main (main) where
 
 
-import Prelude (IO, putStrLn)
+import Prelude (String, IO, putStrLn)
 
 
-import Interpreter.Load (load)
-import Compiler.Counter (Counter(Counter, counter))
+import Interpreter.Load ( load )
+import Compiler.Counter ( Counter(Counter, counter) )
+import System.Environment ( getArgs )
 
 
 main :: IO ()
 main = do
+  args <- getArgs
+  let file'name = module'to'load args
   let cntr = Counter{ counter = 0 }
   putStrLn "Glamorous Glask REPL."
   putStrLn ""
-  load "prelude.glask" cntr
+  load file'name cntr
+
+
+module'to'load :: [String] -> String
+module'to'load (":p" : file'name : _) = file'name
+module'to'load _ = "prelude.glask"
